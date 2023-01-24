2d5dd471c9211175e5ccab67383e2cec3a96f3a4
rkudashev <kudashev.ravil@yandex.ru>
2023-01-24 14:37:54 +0500

    create table Comments

--- app.rb
+++ app.rb
@@ -23,6 +23,15 @@ configure do
 				"created_date" DATE,
 				"content" TEXT
 			)'
+
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+		"Comments"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"created_date" DATE,
+			"content" TEXT,
+			"post_id" INTEGER
+		)'
 end
 
 
