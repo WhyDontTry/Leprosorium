0fe9e1e2e8600ce89d06cefe294e43f8b9ff5fff
rkudashev <kudashev.ravil@yandex.ru>
2023-02-15 15:26:07 +0500

    go to active record

--- Gemfile
+++ Gemfile
@@ -4,3 +4,13 @@ gem "sinatra"
 gem "sinatra-reloader"
 gem "puma"
 gem "sqlite3"
+
+gem "rake"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+
+group :development do
+    gem "tux"
+end
+
--- app.rb
+++ app.rb
@@ -2,43 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
+require 'sinatra/activerecord'
 
-def init_db
-	@db = SQLite3::Database.new 'leprosorium.db'
-	@db.results_as_hash = true
+
+set :database, {adapter: 'sqlite3', database: 'leprosorium.db'}
+
+
+class Post < ActiveRecord::Base
+	validates :content, :presence => true
+	validates :autor, 	:presence => true
 end
 
-before do
-	init_db
+class Comment < ActiveRecord::Base
+	validates :content, :presence => true
 end
 
-configure do
-	init_db
-
-	@db.execute 'CREATE TABLE IF NOT EXISTS
-			"Posts"
-			(
-				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-				"created_date" DATE,
-				"content" TEXT,
-				"autor" TEXT
-			)'
-
-	@db.execute 'CREATE TABLE IF NOT EXISTS
-		"Comments"
-		(
-			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-			"created_date" DATE,
-			"content" TEXT,
-			"post_id" INTEGER
-		)'
-end
 
 
 get '/' do
 
-	@results = @db.execute 'SELECT * FROM Posts ORDER BY id DESC'
+	@posts = Post.order('created_at DESC')
 
 	erb :index			
 end
@@ -48,22 +31,10 @@ get '/new' do
 end
 
 post '/new' do
-	@content = params[:content]
-	autor = params[:autor]
+	post = Post.new params[:post]
+	
+	post.save
 
-	errors = {
-		:content => "Type the text",
-		:autor => "Type autor's name"
-	}
-
-	errors.each do |key, value|
-		if params[key] == ''
-			@error = errors[key]
-			return erb :new
-		end
-	end
-
-	@db.execute 'INSERT INTO Posts (content, created_date, autor) VALUES (?, datetime(), ?)', [@content, autor]
 
 	redirect to '/'
 end
@@ -75,11 +46,10 @@ end
 get '/details/:id' do
 	post_id = params[:id]
 
-	results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
-	@row = results[0]
+	@post = Post.where(id: post_id)
+	@post = @post.first
 
-	@comments = @db.execute 'SELECT * FROM Comments 
-					WHERE post_id = ? ORDER BY id', [post_id]
+	@comments = Comment.where(post_id: post_id).order(created_at: :desc)
 
 	erb :details
 end
@@ -88,22 +58,9 @@ post '/details/:id' do
 	post_id = params[:id]
 	content = params[:content]
 
-	if content.length <= 0
-		@error = "Type the comment"
-		results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
-		@row = results[0]
 
-		@comments = @db.execute 'SELECT * FROM Comments 
-						WHERE post_id = ? ORDER BY id', [post_id]
-		return erb :details
-	end
-
-	@db.execute 'INSERT INTO Comments 
-		(
-			content, 
-			created_date, 
-			post_id
-		) VALUES (?, datetime(), ?)', [content, post_id]
+	comment = Comment.new(post_id: post_id, content: content)
+	comment.save
 
 	redirect to('/details/'.concat post_id)
 end
